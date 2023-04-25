191007ac6ceb4f29caeed5a9dfbb469bb77d007d
Kate Hynes <kahynes@davidson.edu>
2023-04-25 10:36:36 -0400

    adjusting intro text & font size, hiding/showing buttons

--- Assets/Scripts/Text/OpeningText.cs
+++ Assets/Scripts/Text/OpeningText.cs
@@ -10,13 +10,20 @@ public class OpeningText : MonoBehaviour
     public int dialogueIndex = 0;
 
     public string playerName = "PLAYER";
-    public string paragraph3 = "We cannot allow these traitors to continue unchecked. In exchange for your military assistance in suppressing these riots, you will receive great rewards.";
+    public string paragraph3 = "We cannot allow these traitors to continue unchecked. In exchange for your military assistance in suppressing these riots, you will be rewarded generously in gold. You can use that gold to invest in my kingdom and construct new buildings, which will also grant you money and other benefits.";
     public string paragraph4 = "As we embark upon this journey together, let us remember that our nations are stronger united than divided. May the blessings of the divine guide us and protect us in our quest for greatness. Long live the Kingdom of Drodemore and the Kingdom of Zoba!";
+
+    public GameObject continueButton;
+    public GameObject turnPageButton;
 
     public static OpeningText inst;
 
     void Awake() {
         inst = this;
+        continueButton = GameObject.Find("ContinueButton");
+        turnPageButton = GameObject.Find("TurnPageButton");
+        continueButton.SetActive(false);
+
         letterText.text = string.Format("Greetings, {0}. It is I, King Edmund Hamlin XVII, writing to thee from the great halls of my castle. I hope that thou art in good health and spirits. I have heard news of your arrival to the district of Tunstead. The Kingdom of Drodemore expresses their gratitude for our closest ally, the Kingdom of Zoba, for its assistance in our time of need.", playerName);
     }
 
@@ -28,6 +35,8 @@ public class OpeningText : MonoBehaviour
             letterText.text = paragraph3;
         }
         if(dialogueIndex == 3) {
+            continueButton.SetActive(true);
+            turnPageButton.SetActive(false);
             letterText.text = paragraph4;
         }
     }
