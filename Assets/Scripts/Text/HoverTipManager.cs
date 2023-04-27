8db5b21a2f6ed0106d907a47470719daf8a11baf
dyameres <dyameres@davidson.edu>
2023-04-27 10:44:53 -0400

    tooltip

--- Assets/Scripts/Text/HoverTipManager.cs
+++ Assets/Scripts/Text/HoverTipManager.cs
@@ -33,7 +33,7 @@ public class HoverTipManager : MonoBehaviour
         tipWindow.sizeDelta = new Vector2(tipText.preferredWidth > 125 ? 125 : tipText.preferredWidth, tipText.preferredHeight);
 
         tipWindow.gameObject.SetActive(true);
-        tipWindow.transform.position = new Vector2(mousePos.x + tipWindow.sizeDelta.x * 2, mousePos.y);
+        tipWindow.transform.position = new Vector2(mousePos.x + tipWindow.sizeDelta.x /2, mousePos.y + tipWindow.sizeDelta.y /2);
 
     }
 
